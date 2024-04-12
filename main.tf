0cbc536e99b1928a3f9aaa3bee706fe03e2103f0
theppasiva <theppasiva@gmail.com>
2024-04-12 20:17:45 +0530

    vpc-test

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
 module "roboshop" {
-    source = "../terraform-aws-vpc"
-    project_name = var.project_name
-    environment = var.environment
-    common_tags = var.common_tags
-    vpc_tags = var.vpc_tags
+  source = "../terraform-aws-vpc"
+  project_name = var.project_name
+  environment = var.environment
+  common_tags = var.common_tags
+  vpc_tags = var.vpc_tags
 
-    # public subnet
-    public_subnet_cidr = var.public_subnet_cidr
+  # public subnet
+  public_subnets_cidr = var.public_subnets_cidr
 
-    # private subnet
-    private_subnet_cidr = var.private_subnet_cidr
+  # private subnet
+  private_subnets_cidr = var.private_subnets_cidr
+
+  # database subnet
+  database_subnets_cidr = var.database_subnet_cidr
+
+  # peering
+  is_peering_required = var.is_peering_required
+
 }
--- variables.tf
+++ variables.tf
@@ -24,10 +24,18 @@ variable "environment" {
     default = "dev"
 }
 
-variable "public_subnet_cidr" {
-    default = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "public_subnets_cidr" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "private_subnet_cidr" {
-    default = ["10.0.11.0/24", "10.0.12.0/24"]
+variable "private_subnets_cidr" {
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "database_subnet_cidr" {
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "is_peering_required" {
+  default = true
 }
